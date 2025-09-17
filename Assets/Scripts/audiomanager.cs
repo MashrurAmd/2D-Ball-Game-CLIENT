27b09d2f67ecf0deea2823de72e441dac09eedad
Mashrur Ahmed <mashrurahmed500@gmail.com>
2025-09-17 15:22:55 +0600

    s

---
+++ Assets/Scripts/audiomanager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiomanager : MonoBehaviour
+{
+    [Header("---------------------Audio Sound--------------")]
+    [SerializeField]  AudioSource musicSource;
+    [SerializeField]  AudioSource SFX;
+
+    [Header("---------------------Audio Clip--------------")]
+    public AudioClip background;
+    public AudioClip correctColor;
+    public AudioClip wrongColor;
+
+
+
+
+
+}
